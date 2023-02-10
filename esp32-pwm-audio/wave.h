b659e2d082dc01be34499a4f650af529d185c5e4
alanbjohnston <alan.b.johnston@gmail.com>
2023-02-10 14:20:03 -0500

    moved file

---
+++ esp32-pwm-audio/wave.h
@@ -0,0 +1,17 @@
+
+
+#ifndef _PWM_AUDIO_WAVE_H_
+#define _PWM_AUDIO_WAVE_H_
+
+
+char *wave_get(void);
+
+uint32_t wave_get_size(void);
+
+uint32_t wave_get_framerate(void);
+
+uint32_t wave_get_bits(void);
+
+uint32_t wave_get_ch(void);
+
+#endif
